01c3f0e22a65fadc26e8acd3c987f1903630bd0e
andreresende36 <andreresende36@gmail.com>
2023-09-04 19:28:32 -0300

    fix lint

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,8 +32,10 @@ def scrape_next_page_link(html_content):
 
 
 # headers = {
-#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,
+# image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+# AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
 # }
 # text = requests.get(
 #     "https://blog.betrybe.com/tecnologia/jogos-iniciantes-aprender-programar/",
