68944fd8961949d1b08b5b0d52cc117257b08ff6
andreresende36 <andreresende36@gmail.com>
2023-09-04 19:23:34 -0300

    feature 4: scrape_news done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 from time import sleep
 from parsel import Selector
+from bs4 import BeautifulSoup
+import re
 
 
 # Requisito 1
@@ -29,10 +31,56 @@ def scrape_next_page_link(html_content):
     return selector.css(".next::attr(href)").get()
 
 
+# headers = {
+#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+# }
+# text = requests.get(
+#     "https://blog.betrybe.com/tecnologia/jogos-iniciantes-aprender-programar/",
+#     headers=headers,
+# ).text
+# with open("text.html", "w") as file:
+#     file.write(text)
+
+
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    url_element = soup.find(rel="canonical")
+    url = url_element["href"] if url_element else None  # type: ignore
+    title_element = soup.find("h1")
+    title = title_element.text.replace("\xa0", "") if title_element else None
+    timestamp_element = soup.find("li", {"class": "meta-date"})
+    timestamp = timestamp_element.text if timestamp_element else None
+    writer_parent = soup.find("span", {"class": "author"})
+    writer = writer_parent.a.text if writer_parent else None  # type: ignore
+    reading_time_element = soup.find("li", {"class": "meta-reading-time"})
+    reading_time = (
+        int(re.findall(r"\d+", reading_time_element.text)[0])
+        if reading_time_element
+        else None
+    )
+    summary_parent = soup.find("div", {"class": "entry-content"})
+    summary = (
+        summary_parent.p.text.replace("\xa0", "").removesuffix(  # type: ignore
+            " "
+        )
+        if summary_parent
+        else None
+    )
+    category_element = soup.find("span", {"class": "label"})
+    category = category_element.text if category_element else None
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
